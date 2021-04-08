9330735108fe91b66a1d897a89ff3589ac29e5ed
David Yonehara <davidyonehara@gmail.com>
2021-04-07 23:18:44 -0700

    add typescript

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * isEven - Determines if a number is even or not. Returns false if a non-integer is passed in.
+ * 
+ * @param {number} int - number to check for evenness.
+ * @returns {boolean}
+ */
+
+export const isEven = ( int: Number ): Boolean => {
+    return (typeof int === "number") 
+        && Number.isFinite(int)
+        && !(int%2);
+}
